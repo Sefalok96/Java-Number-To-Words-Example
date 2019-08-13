2c911e0606750720eb13d42e1c69da6d99baafaf
Sefa <49784131+Sefalok96@users.noreply.github.com>
2019-08-13 18:40:30 +0300

    Update main.java

--- DiagonalStar/src/main.java
+++ DiagonalStar/src/main.java
@@ -1,7 +1,6 @@
 public class main {
     public static void main(String[] args){
-        //printSquareStar(8);
-        //print_squaredi(6);
+     
         printSquareStar(2);
     }
 
@@ -31,38 +30,9 @@ public class main {
 
         }
     }
-    public static void print_squaredi(int n)
-    {
-        int i, j;
-        for (i = 1; i <= n; i++)
-        {
-            for (j = 1; j <= n; j++)
-            {
-                if (i == 1 || i == n || j == 1 ||
-                        j == n || i == j || j == (n - i + 1))
-                    System.out.print("*");
-                else
-                    System.out.print(" ");
-            }
-            System.out.println();
-        }
+    
 
-    }
-
-    public static void Printet(int num){
-        for(int row=1;row<=num;row++){
-            for(int col=1;col<=num;col++){
-                if(row==num || col==num || row==1 || col==1 || row==col || col==(num-row+1)) {
-
-                    System.out.print("*");
-                }
-
-                 else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
+   
     }
 
 
